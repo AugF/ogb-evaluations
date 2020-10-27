53d1b2e645419566a3bb6f5f9a5d0a486d106ba8
AugF <wangyp@smail.nju.edu.cn>
2020-10-27 23:23:17 +0800

    ogbn baselines完成

---
+++ ogbn/log/run_baselines.py
@@ -0,0 +1,14 @@
+import os
+import random
+
+os.chdir("/home/wangzhaokang/wangyunpan/gnns-project/ogb_evaluations/ogbn")
+for data in ['mag', 'products']:
+    for sampler in ['neighborsampling', 'cluster', 'graphsaint']:
+        for alg in ['gcn', 'sage', 'gat', 'rgcn']:
+            file_path = f"{data}/{sampler}_{alg}.py"
+            if not os.path.exists(file_path):
+                continue
+            print(file_path)
+            cmd = f"nohup python -u {file_path} --device {random.randint(0,1)} 1>log/{data}_{sampler}_{alg}.out 2>&1 &"
+            os.system(cmd)
+
