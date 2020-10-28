dd1038d3b0e3983e456a2c581017fecb5a55389d
AugF <wangyp@smail.nju.edu.cn>
2020-10-28 17:30:31 +0800

    增加了一些过滤的文件

--- ogbn/log/run_baselines.py
+++ ogbn/log/run_baselines.py
@@ -1,14 +1,27 @@
 import os
-import random
+import sys
+import time
 
 os.chdir("/home/wangzhaokang/wangyunpan/gnns-project/ogb_evaluations/ogbn")
-for data in ['mag', 'products']:
-    for sampler in ['neighborsampling', 'cluster', 'graphsaint']:
-        for alg in ['gcn', 'sage', 'gat', 'rgcn']:
-            file_path = f"{data}/{sampler}_{alg}.py"
-            if not os.path.exists(file_path):
-                continue
-            print(file_path)
-            cmd = f"python -u {file_path} --device {random.randint(0,1)} 1>log/{data}_{sampler}_{alg}.out 2>&1"
-            os.system(cmd)
+
+if len(sys.argv) < 3:
+    print("python run_baselines.py data device-id")
+    sys.exit(0)
+    
+data = sys.argv[1]
+device_id = sys.argv[2]
+
+# for data in ['mag', 'products']:
+for sampler in ['neighborsampling', 'cluster', 'graphsaint']:
+    for alg in ['gcn', 'sage', 'gat', 'rgcn']:
+        file_path = f"{data}/{sampler}_{alg}.py"
+        if not os.path.exists(file_path):
+            continue
+        print(file_path)
+        st = time.time()
+        cmd = f"python -u {file_path} --device {device_id} 1>log/{data}_{sampler}_{alg}.out 2>&1"
+        os.system(cmd)
+        ed = time.time()
+        print(f"{time.strftime('%Y.%m.%d',time.localtime(ed))}, use_time: {ed - st}")
+        
 
